78ef59e51305dc706d4eb5ce04aac465cb4a42dd
ElRaptorus <christian.werner@5minds.de>
2018-12-14 14:29:40 +0100

    ⬆️ Upgrade test setup
    **Changes:**
    
    - Upgrade ProcessEngine and ProcessEngineContracts
    - Globally use Bluebird as the default Promise library
    - Reduce the polling interval used by the `ProcessInstanceHandler` when waiting for a suspended task.

--- _integration_tests/src/fixture_providers/process_instance_handler.ts
+++ _integration_tests/src/fixture_providers/process_instance_handler.ts
@@ -34,8 +34,8 @@ export class ProcessInstanceHandler {
 
   public async waitForProcessInstanceToReachSuspendedTask(correlationId: string, processModelId?: string): Promise<void> {
 
-    const maxNumberOfRetries: number = 30;
-    const delayBetweenRetriesInMs: number = 500;
+    const maxNumberOfRetries: number = 60;
+    const delayBetweenRetriesInMs: number = 200;
 
     const flowNodeInstanceService: IFlowNodeInstanceService = this.testFixtureProvider.resolve<IFlowNodeInstanceService>('FlowNodeInstanceService');
 
@@ -61,8 +61,8 @@ export class ProcessInstanceHandler {
 
   public async waitForExternalTaskToBeCreated(topicName: string, maxTask: number = 100): Promise<void> {
 
-    const maxNumberOfRetries: number = 30;
-    const delayBetweenRetriesInMs: number = 500;
+    const maxNumberOfRetries: number = 60;
+    const delayBetweenRetriesInMs: number = 200;
 
     const externalTaskRepository: IExternalTaskRepository = this.testFixtureProvider.resolve<IExternalTaskRepository>('ExternalTaskRepository');
 
