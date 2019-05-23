3220cd99181d8a0ccc0fe24a4786590d2e808829
ElRaptorus <christian.werner@5minds.de>
2019-05-23 11:27:47 +0200

    :package: Improve reinstall script to include the .net packages

--- reinstall.sh
+++ reinstall.sh
@@ -15,7 +15,39 @@ if [[ "$?" -ne "0" ]]; then
   exit 1;
 fi
 
-# build all packages and schemas
+# build all packages
+echo "Building .TS packages"
 meta exec "npm run build" --exclude external_task_api_meta,external_task_api_contracts,external_task_api_client
+
+
+function install_and_build_package {
+  cd typescript
+  npm install --no-package-lock
+  cd ..
+
+  if [ -x "$(command -v dotnet)" ]; then
+    cd dotnet/src
+    dotnet restore && dotnet build
+    cd ../..
+  else
+    echo ""
+    echo "WARNING: skipping dotnet (since it is not installed)."
+    echo ""
+  fi
+}
+
+echo "-------------------------------------------------"
+echo "Installing ExternalTask API Contracts"
+echo "-------------------------------------------------"
+cd external_task_api_contracts
+install_and_build_package
+cd ..
+
+echo "-------------------------------------------------"
+echo "Installing ExternalTask API Client"
+echo "-------------------------------------------------"
+cd external_task_api_client
+install_and_build_package
+cd ..
 
 echo "done"
